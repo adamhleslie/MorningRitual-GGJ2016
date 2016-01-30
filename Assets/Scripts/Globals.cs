82fa1f0b6882e25aafd62c9d066b69f32f8c9ae6
Benjamin Johnson <bencj96@gmail.com>
2016-01-30 12:17:19 -0600

    pray to god this works

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -4,13 +4,10 @@ using System.Collections;
 public class Globals{
     public static bool testingFloor = false;
     public static ArrayList testCollisions = new ArrayList();
-<<<<<<< HEAD:Assets/BensStuff/Globals.cs
 
     public static bool buttonPressed = false;
-=======
     public static int nextScene = 1;
     public static float ritualValue = 0;
->>>>>>> origin/master:Assets/Scripts/Globals.cs
 }
 
 
