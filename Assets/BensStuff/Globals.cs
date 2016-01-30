c5355c5ccd0a4b986b27f9697be325bab935a621
Benjamin Johnson <bencj96@gmail.com>
2016-01-29 22:43:01 -0600

    A texture is a texture no matter how small
    -Gorge Forman

---
+++ Assets/BensStuff/Globals.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Globals{
+    public static bool testingFloor = false;
+	
+}
---
+++ Assets/BensStuff/OnCollision.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnCollision : MonoBehaviour {
+    int numberStay = 0;
+	
+	// Update is called once per frame
+	void OnCollisionEnter(Collision collision)
+    {
+        numberStay++;
+        Debug.Log("Enter");
+	}
+
+    void OnCollisionStay(Collision collision)
+    {
+        if (numberStay > 5)
+        {
+            Globals.testingFloor = true;
+
+        }
+        Debug.Log("Stay" + Globals.testingFloor);
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        numberStay--;
+        if (numberStay <= 5)
+        {
+            Globals.testingFloor = false;
+        }
+        Debug.Log("Exit");
+    }
+}
