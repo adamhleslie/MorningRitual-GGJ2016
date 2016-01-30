5a9b8cf7536f95f90be45d5d31c70990f54ac0cd
chargao <charlie.gao@utexas.edu>
2016-01-30 11:45:20 -0600

    working on radio audio

---
+++ Assets/Scripts/RadioScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Radio : MonoBehaviour {
+  public AudioSource audio;
+  
+	// Use this for initialization
+	void Start () {
+     AudioSource[] audios = GetComponents<AudioSource>();     
+	}
+	
+	// Update is called once per frame
+	// void Update () {
+    
+	// }
+  
+  //will change this to "onButtonPress" 
+  void OnCollisionEnter(Collision c) {
+    
+		if (c.gameObject.name == "Enemy") {
+			clickAudio.Play ();
+		}
+	}
+}
