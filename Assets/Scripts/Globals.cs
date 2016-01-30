2e6b2a3cce08257764ec9c33314fa5f7dc4b02da
Adam Leslie <adamhleslie@gmail.com>
2016-01-30 11:40:53 -0600

    Door Handle, and floor area

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -4,4 +4,6 @@ using System.Collections;
 public class Globals{
     public static bool testingFloor = false;
     public static ArrayList testCollisions = new ArrayList();
+    public static int nextScene = 1;
+    public static float ritualValue = 0;
 }
