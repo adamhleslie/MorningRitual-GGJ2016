d7c3fb12261853a43013b888263fbaf40f9d13f7
Adam Leslie <adamhleslie@gmail.com>
2016-01-31 14:30:09 -0600

    Everythign Working?

--- Assets/Scripts/OnCollision.cs
+++ Assets/Scripts/OnCollision.cs
@@ -3,23 +3,58 @@ using System.Collections;
 
 public class OnCollision : MonoBehaviour {
     int numberStay = 0;
+    public bool dishes;
+    // ArrayList curCollisions = new ArrayList();
+
+    // void Start()
+    // {
+    //     curCollisions = new ArrayList();
+    // }
 
     // Update is called once per frame
     void OnCollisionEnter(Collision collision)
     {
-        numberStay++;
-        Debug.Log("Enter");
-        Globals.testCollisions.Add(collision);
+        if(!dishes || (dishes && collision.gameObject.tag == "Dishes"))
+        {
+            numberStay++;
+        }
+
+        Debug.Log("Entered");
+        if(dishes && numberStay == 2)
+        {
+            Debug.Log("Dishes DONE");
+            Globals.ritualSequence[1] = true;
+        }
+        else if (!dishes && numberStay == 5)
+        {
+            Debug.Log("TRASHCAN DONE");
+            Globals.ritualSequence[3] = true;
+        }
 	}
 
-    void OnCollisionStay(Collision collision)
-    {
-        if (Globals.testCollisions.Count > 5)
-        {
-            Globals.testingFloor = true;
-        }
-        Debug.Log("Stay" + Globals.testingFloor + Globals.testCollisions.Count);
-    }
+    // void OnCollisionStay(Collision collision)
+    // {
+    //     if (dishes && !((bool)Globals.ritualSequence[1]) && curCollisions.Count >= 2)
+    //     {
+    //         int numDishes = 0;
+    //         for(int i = 0; i < curCollisions.Count && numDishes < 2; i++)
+    //         {
+    //             if(((Collision)curCollisions[i]).gameObject.tag == "Dishes")
+    //             {
+    //                 numDishes++;
+    //             }
+    //         }
+    //         if(numDishes == 2){
+
+    //         }
+    //     }
+    //     else if (!dishes && !((bool)Globals.ritualSequence[3]) && curCollisions.Count > 5)
+    //     {
+    //         Debug.Log("TRASHCAN DONE");
+    //         Globals.ritualSequence[3] = true;
+    //     }
+    //     //Debug.Log("Stay" + Globals.testingFloor + Globals.testCollisions.Count);
+    // }
 
     void OnCollisionExit(Collision collision)
     {
